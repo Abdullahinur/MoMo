9ee24a916cb547c3be744d803c39d667f3d18de3
Abdullahinur <aabdullahinur@hotmail.com>
2018-06-01 13:14:33 +0300

    First Commit: Create file structure, connect to postgres install psycopg2

---
+++ virtual/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/abdullahinur/Documents/School/Core/Prototypes/Django/MoMo/virtual/bin/python3.6
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
