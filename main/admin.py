7464a0c6d65f88ca4c9914b22014c4836c48a385
Abdullahinur <aabdullahinur@hotmail.com>
2018-06-03 21:18:43 +0300

    register models in admin

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import (Contact, Conversation, User, User_Options, User_Photo,
+                     User_Posts)
+
 # Register your models here.
+admin.site.register(User)
+admin.site.register(User_Photo)
+admin.site.register(User_Posts)
+admin.site.register(User_Options)
+admin.site.register(Contact)
+admin.site.register(Conversation)
