952175f98e19b51b5cf5075feb23da6d322633b8
Abdullahinur <aabdullahinur@hotmail.com>
2018-06-03 22:10:57 +0300

    Fix issues with profile view: 	error null str

--- virtual/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/home/abdullahinur/Documents/School/Core/Prototypes/Django/MoMo/virtual/bin/python3.6
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
